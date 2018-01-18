a92a5fb81fdf41b2da6630c5a4977d720cce7025
Leo <asdfpeng@qq.com>
2018-01-18 14:44:15 +0800

    deffer

--- ES6/async/Q.js
+++ ES6/async/Q.js
@@ -37,3 +37,94 @@ just like try, catch, and finally.
 An error in promisedStep1 will flow all the way to the catch function, 
 where it’s caught and handled. (Here promisedStepN is a version of stepN that returns a promise.)
 */
+
+/*
+Note that if you have a method that uses the Node.js callback pattern, 
+as opposed to just a function, you will need to bind its this value before passing it to nfbind, 
+like so:
+*/
+var Kitty = mongoose.model("Kitty");
+var findKitties = Q.nfbind(Kitty.find.bind(Kitty));
+
+// 再如
+var Batch = require("../models").Batch;
+Batch.qFindOne = q.nbind(Batch.findOne, Batch);
+
+exports.findById = function(id, cb) {
+	const batchInfo = {};
+	Batch.qFindOne({_id: id})
+		.then(function(data) {
+			if (data) {
+				batchInfo = data;
+				return q.resolve();
+			} else {
+				batchInfo = null;
+				return q.resolve();
+			}
+		})
+		.then(function(data) {
+			if (data && data.name) {
+				batchInfo.yield = data.yield;
+			}
+			cb && cb(null,batchInfo);
+		})
+		.catch(function(err) {
+			cb && cb(err);
+		})
+}
+
+var batchService = require("../../service/batch");
+
+Q.nfcall(batchService.findById, batch_id)
+	.then(function(batch) {
+		if (!batch) {
+			throw err;
+		}
+		return batch;
+	})
+	.then(function(batch) {
+		var deferred = q.defer();
+		if (batch && batch.limit) {
+			if (err) return deferred.reject(err);
+			deferred.resolve(batch);
+		} else {
+			deferred.resolve(batch);
+		}
+		return deferred.promise;
+	})
+
+// Using Deferreds
+// If you have to interface with asynchronous functions that are callback-based instead of promise-based, 
+//Q provides a few shortcuts (like Q.nfcall and friends). But much of the time, the solution will be to use deferreds.
+var deferred = Q.defer();
+FS.readFile("foo.txt", "utf-8", function (error, text) {
+    if (error) {
+        deferred.reject(new Error(error));
+    } else {
+        deferred.resolve(text);
+    }
+});
+return deferred.promise;
+
+// Note that a deferred can be resolved with a value or a promise. 
+// The reject function is a shorthand for resolving with a rejected promise.
+// this:
+deferred.reject(new Error("Can't do it"));
+
+// is shorthand for:
+var rejection = Q.fcall(function () {
+    throw new Error("Can't do it");
+});
+deferred.resolve(rejection);
+
+/*
+Adapting Node
+If you're working with functions that make use of the Node.js callback pattern, 
+where callbacks are in the form of function(err, result), 
+Q provides a few useful utility functions for converting between them. 
+The most straightforward are probably Q.nfcall and Q.nfapply ("Node function call/apply") 
+for calling Node.js-style functions and getting back a promise:
+*/
+return Q.nfcall(FS.readFile, "foo.txt", "utf-8");
+return Q.nfapply(FS.readFile, ["foo.txt", "utf-8"]);
+
