53c74b059d337d793fcb0b9b4bd726dc07cc9b0f
Leo <asdfpeng@qq.com>
2018-01-31 14:34:54 +0800

    Implementing Container Components

--- ES6/Array/filter.js
+++ ES6/Array/filter.js
@@ -1,3 +1,4 @@
+// 例1
 function isBigEnough(value) {
   return value >= 10;
 }
@@ -15,3 +16,19 @@ let [...spraed]= [12, 5, 8, 130, 44];
 let filtered = spraed.filter(isBigEnough);
 
 // filtered is [12, 130, 44]
+
+
+// 例2
+var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6);
+
+console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]
+
+// ES6 version
+const words = ["spray", "limit", "elite", "exuberant", "destruction", "present", "happy"];
+
+let longWords = words.filter(word => word.length > 6);
+
+// Filtered array longWords is ["exuberant", "destruction", "present"]
