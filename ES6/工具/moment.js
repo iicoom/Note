e562393ccba1c5ded41090ae6ce4981f844121db
Leo <asdfpeng@qq.com>
2018-02-01 16:58:15 +0800

    moment

--- ES6/工具/moment.js
+++ ES6/工具/moment.js
@@ -16,6 +16,9 @@ console.log( Date.parse('2017-12-15T08:30:50.913Z'))
 
 console.log("======moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')========")
 console.log( moment(Date.now()).format('YYYY-MM-DD HH:mm:ss') )
+moment(record.create_at).format('YYYY-MM-DD HH:mm:ss')
+// 2017-12-15T08:30:50.913Z =>
+// 2018-02-01 10:16:24
 
 console.log("======moment(1513326650913).format('YYYY-MM-DD')========")
 console.log( moment(1513326650913).format('YYYY-MM-DD') )
