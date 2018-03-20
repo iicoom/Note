e2f0e01ef0e32d85aa97f29e2cabaf8bc328b5fb
Leo <asdfpeng@qq.com>
2018-03-20 11:05:37 +0800

    _.reduce

---
+++ Lodash/3.8._reduce.js
@@ -0,0 +1,10 @@
+_.reduce([1, 2], function(sum, n) {
+  return sum + n;
+}, 0);
+// => 3
+ 
+_.reduce({ 'a': 1, 'b': 2, 'c': 1 }, function(result, value, key) {
+  (result[value] || (result[value] = [])).push(key);
+  return result;
+}, {});
+// => { '1': ['a', 'c'], '2': ['b'] } (iteration order is not guaranteed)
