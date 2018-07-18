2c7cb30e7c590ef16af4d10f183f11dd8a0e64de
Leo <asdfpeng@qq.com>
2018-07-18 16:02:54 +0800

    shift search match

--- ES6/Array/unshift.js
+++ ES6/Array/unshift.js
@@ -4,3 +4,20 @@ var fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.unshift("Lemon","Pineapple");
 
 // Lemon,Pineapple,Banana,Orange,Apple,Mango
+
+// shift
+var arr = new Array(3)
+arr[0] = "George"
+arr[1] = "John"
+arr[2] = "Thomas"
+
+console.log(arr)
+["George", "John", "Thomas"]
+
+arr.shift()
+"George"
+arr
+["John", "Thomas"]
+
+
+要把一个或者多个元素添加到数组的尾部,请使用push()
