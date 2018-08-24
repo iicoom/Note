821c62cf20357b0762d8cdec2de3ce29eb1cbd15
Leo <asdfpeng@qq.com>
2018-08-24 15:27:30 +0800

    node.js util.inspect

---
+++ Nodejs/Util/inspect.js
@@ -0,0 +1,24 @@
+const util = require('util');
+
+let payInfo = {
+        uid: 'openId',
+        jifen: 'reqContent.jifen' || 0, // 一个羊角抵扣一元
+        balance: 'reqContent.balance' || 0,
+        pay_pwd: 'reqContent.pay_pwd' || '',
+        other_pay: {
+            //type: reqContent.other_pay_type,
+            amount: 'reqContent.other_pay_amount' || 0
+        },
+        payer_ip: 'payer_ip',
+        client_ua: 'client_ua',
+        //wxopen_id: req.session.wx_openid || ''
+    };
+
+
+console.log("payInfo:\n", payInfo)
+console.log("typeof payInfo:", typeof(payInfo)) // object
+console.log("util.inspect(payInfo):\n", util.inspect(payInfo))
+console.log("typeof util.inspect(payInfo):", typeof(util.inspect(payInfo))) // string
+
+// util.inspect(object[, options])
+console.log("util.inspect(payInfo):\n", util.inspect(payInfo, {colors: true}))
