110e98e02cba7a5501697f449511024c22d0bbd6
Leo <asdfpeng@qq.com>
2018-09-01 22:24:12 +0800

    weixin 授权

---
+++ ES6/Promise/q.js
@@ -0,0 +1,58 @@
+// 整体为一个立即执行表达式
+
+/*
+	（1）分情况暴露出Q API 
+		在Node.js环境下
+		
+		在浏览器环境下
+		typeof window
+		=> "object"
+		typeof self
+		=> "object"
+
+*/
+
+(function (definition) {
+	"use strict";
+
+	// This file will function properly as a <script> tag, or a module
+    // using CommonJS and NodeJS or RequireJS module formats.  In
+    // Common/Node/RequireJS, the module exports the Q API and when
+    // executed as a simple <script>, it creates a Q global instead.
+
+     // Montage Require
+    if (typeof bootstrap === "function") {
+        bootstrap("promise", definition);
+
+    // CommonJS
+    } else if (typeof exports === "object" && typeof module === "object") {
+        module.exports = definition();
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+        define(definition);
+
+    // <script>
+    } else if (typeof window !== "undefined" || typeof self !== "undefined") {
+        // Prefer window over self for add-on scripts. Use self for
+        // non-windowed contexts.
+        var global = typeof window !== "undefined" ? window : self;
+
+        // Get the `window` object, save the previous Q global
+        // and initialize Q as a global.
+        var previousQ = global.Q;
+        global.Q = definition();
+
+        // Add a noConflict function so Q can be removed from the
+        // global namespace.
+        global.Q.noConflict = function () {
+            global.Q = previousQ;
+            return this;
+        };
+
+    } else {
+        throw new Error("This environment was not anticipated by Q. Please file a bug.");
+    }
+})(function () {
+
+})
