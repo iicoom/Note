39f4b39a12086e472223e1b93a62919e9fdb8998
Leo <asdfpeng@qq.com>
2018-09-04 11:42:21 +0800

    .pulll

---
+++ Lodash/Array/_.pull.js
@@ -0,0 +1,19 @@
+var array = ['a', 'b', 'c', 'a', 'b', 'c'];
+ 
+_.pull(array, 'a', 'c');
+console.log(array);
+// => ['b', 'b']
+
+
+var array = ['a', 'b', 'c', 'a', 'b', 'c'];
+ 
+_.pullAll(array, ['a', 'c']);
+console.log(array);
+// => ['b', 'b']
+
+
+var array = [{ 'x': 1 }, { 'x': 2 }, { 'x': 3 }, { 'x': 1 }];
+ 
+_.pullAllBy(array, [{ 'x': 1 }, { 'x': 3 }], 'x');
+console.log(array);
+// => [{ 'x': 2 }]
