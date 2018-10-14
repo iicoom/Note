3840549e07a28c6ed81e81b13e04b751516189f9
Leo <asdfpeng@qq.com>
2018-10-14 10:16:00 +0800

    lodash group

--- Lodash/3.7._groupBy.js
+++ Lodash/3.7._groupBy.js
@@ -26,3 +26,22 @@ console.log(ogroup)
 //    [ { user_id: '123', other: 'jjj' },
 //      { user_id: '456', other: 'jjj' } ],
 //   kkk: [ { user_id: '123', other: 'kkk' } ] }
+
+
+
+const originalData = [
+  { name: 'bo', age: 30, score: 90, gender: 1, lesson: 'math' },
+  { name: 'hou', age: 31, score: 80, gender: 1, lesson: 'math' }, 
+  { name: 'jian', age: 27, score: 70, gender: 0, lesson: 'math' }, 
+  { name: 'bo', age: 30, score: 80, gender: 1, lesson: 'english' }
+];
+
+// const group = _.groupBy(originalData,'lesson');
+// console.log(group)
+// { math:
+//    [ { name: 'bo', age: 30, score: 90, gender: 1, lesson: 'math' },
+//      { name: 'hou', age: 31, score: 80, gender: 1, lesson: 'math' },
+//      { name: 'jian', age: 27, score: 70, gender: 0, lesson: 'math' } ],
+//   english:
+//    [ { name: 'bo', age: 30, score: 80, gender: 1, lesson: 'english' } ] 
+// }
