07d5b57e4fca06f57ef07cfef4b4d019d9d13a48
Leo <asdfpeng@qq.com>
2019-03-17 11:35:40 +0800

    reg arg

---
+++ JavaScript/function/arguments.js
@@ -0,0 +1,15 @@
+// arguments 是一个对应于传递给函数的参数的类数组对象。
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Functions/arguments
+
+function testArg() {
+	console.log(arguments)
+}
+
+testArg('qq',123,[4,5,6],{'key':'value'})
+
+Arguments(4) ["qq", 123, Array(3), {…}, callee: ƒ, Symbol(Symbol.iterator): ƒ]
+
+0: "qq"
+1: 123
+2: (3) [4, 5, 6]
+3: {key: "value"}
