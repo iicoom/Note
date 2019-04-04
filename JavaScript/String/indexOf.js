435f04bd95bba66c93755616fdcf76a5a5f276ee
Leo <asdfpeng@qq.com>
2019-04-04 14:16:54 +0800

    js method

--- JavaScript/String/indexOf.js
+++ JavaScript/String/indexOf.js
@@ -1,6 +1,21 @@
+/*
+indexOf只匹配到第一个合适的，把位置返回给你。
+
+lastIndexOf是匹配最后一个合适的，把位置返回给你。
+位置从0开始算的
+*/
+
 'hillj'.indexOf('h')
 // => 0
 'hillj'.indexOf('')
 // => 0
 'hillj'.indexOf('k')
 // => -1
+
+'sad.sad.dsd.'.lastIndexOf('.')
+11
+'sad.sad.dsd.'.indexOf('.')
+3
+
+'file.dsfa.txt'.slice('file.dsfa.txt'.lastIndexOf('.'))
+".txt"
--- JavaScript/String/split-join.js
+++ JavaScript/String/split-join.js
@@ -6,28 +6,28 @@
 
 
 /**
-   * 解析URL参数
-   *
-   * @static
-   * @param {any} url
-   * @returns
-   * @memberof Utility
-   */
-  function parseUrl(url) {
-    if (!url) {
-      return {};
-    }
-    const obj = {};
-    url.replace('?', '').split('&').forEach((keyValue) => {
-      const arr = keyValue.split('=');
-      const key = arr[0];
-      const value = arr[1];
-      obj[key] = value;
-    });
-    return obj;
+ * 解析URL参数
+ *
+ * @static
+ * @param {any} url
+ * @returns
+ * @memberof Utility
+ */
+function parseUrl(url) {
+  if (!url) {
+    return {};
   }
+  const obj = {};
+  url.replace('?', '').split('&').forEach((keyValue) => {
+    const arr = keyValue.split('=');
+    const key = arr[0];
+    const value = arr[1];
+    obj[key] = value;
+  });
+  return obj;
+}
 
-  const activityUrl = "?activity_id=5a2b943775930f7b04fc47f9&skip=0&limit=15";
+const activityUrl = "?activity_id=5a2b943775930f7b04fc47f9&skip=0&limit=15";
 
 console.log("=========activityUrl.replace('?', '').split('&')=========")
 console.log(activityUrl.replace('?', '').split('&'))
@@ -53,3 +53,4 @@ console.log( parseUrl(activityUrl) )
 
 '1000'.split('')
 (4) ["1", "0", "0", "0"]
+
