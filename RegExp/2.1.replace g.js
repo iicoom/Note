ebae7ccbb3367fbbe68cda65ef430a331574480d
Leo <asdfpeng@qq.com>
2019-12-10 16:09:39 +0800

    Update 2.1.replace g.js

--- RegExp/2.1.replace g.js
+++ RegExp/2.1.replace g.js
@@ -1,6 +1,15 @@
+// ## 替换特殊符号
 'dafa++fasdf+'.replace(/\+/g, ' ')
 // "dafa  fasdf "
 'dafa++fasdf+'.replace(/\+/g, '')
 // "dafafasdf"
 'dafa++fasdf+'.replace(/\+/, '')
 // "dafa+fasdf+"
+
+
+// trim 字符串空白字符
+let str = '[  {"id":11111  23,"count":123}, {   "id":1111123,"count":213}]';
+function trim(str) {
+ str.replace(/\s+/g, "")
+}
+// [{"id":1111123,"count":123},{"id":1111123,"count":213}]
