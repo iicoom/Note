2bd781c8ab12e909690d8751f9b8287ebecc0560
Leo <asdfpeng@qq.com>
2020-02-18 16:22:59 +0800

    Update and rename forof.js to for-forin-forof.js

--- JavaScript/Array/遍历数组/for-forin-forof.js
+++ JavaScript/Array/遍历数组/for-forin-forof.js
@@ -1,3 +1,6 @@
+/**
+for in
+*/
 const obj = {
 	'a': '1',
 	'b': '2'
@@ -8,12 +11,14 @@ for ( const prop in obj) {
 }
 
 
+/**
+for of
+*/
 const arr = [{'a':'1'},{'a':'2'}]
 for ( const index of arr) {
 	index.b = '3'
 	console.log(index)
 }
-
 /*
 =>
 obj.a = 1
@@ -22,3 +27,12 @@ obj.b = 2
 { a: '2', b: '3' }
 */
 //for in 操作对象的，for of 、forEach操作数组
+
+/**
+for
+*/
+for (let i = 0; i < arr.length; i ++) {
+   console.log(arr[i].a)
+}
+// 1
+// 2
