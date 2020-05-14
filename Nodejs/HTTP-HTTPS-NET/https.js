ba9eb7810a4678ff72a83f3cf90514b5f4195ba7
Xiaojie Mao <maoxiaojie@nextjoy.com>
2020-05-14 17:36:25 +0800

    why node

--- Nodejs/HTTP-HTTPS-NET/https.js
+++ Nodejs/HTTP-HTTPS-NET/https.js
@@ -5,25 +5,25 @@ const https = require('https');
 const fs = require('fs');
 
 const options = {
-  key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
-  cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
+  key: fs.readFileSync('certificate/server.key'),
+  cert: fs.readFileSync('certificate/kart_com.crt')
 };
 
 https.createServer(options, (req, res) => {
   res.writeHead(200);
   res.end('hello world\n');
-}).listen(8000);
+}).listen(8900);
 
 
 
-https.get('https://www.baidu.com/', (res) => {
-  console.log('状态码:', res.statusCode);
-  console.log('请求头:', res.headers);
+// https.get('https://www.baidu.com/', (res) => {
+//   console.log('状态码:', res.statusCode);
+//   console.log('请求头:', res.headers);
 
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
+//   res.on('data', (d) => {
+//     process.stdout.write(d);
+//   });
 
-}).on('error', (e) => {
-  console.error(e);
-});
+// }).on('error', (e) => {
+//   console.error(e);
+// });
