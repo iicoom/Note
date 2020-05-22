3a5cec656c1041225b2d283d0bc78e66701258c3
Xiaojie Mao <maoxiaojie@nextjoy.com>
2020-05-22 14:24:04 +0800

    os

---
+++ Nodejs/OS/os.hostname.js
@@ -0,0 +1,10 @@
+const os = require("os");
+
+console.log(os.hostname())
+// MXJ-PC
+console.log(os.platform())
+// win32 
+console.log(os.release())
+// 10.0.18362
+console.log(os.uptime())
+// 5164
