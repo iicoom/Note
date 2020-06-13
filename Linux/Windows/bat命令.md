431ce3ae37f2ae4dd059df92a1b752b89ff71027
Leo <asdfpeng@qq.com>
2020-06-13 19:11:55 +0800

    log

--- Linux/Windows/bat命令.md
+++ Linux/Windows/bat命令.md
@@ -2,3 +2,6 @@
 
 ## [使用批处理脚本 在特定目录中启动Git-Bash窗口](https://blog.csdn.net/qq_21808961/article/details/86026577?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase)
 
+## CHOICE
+https://www.robvanderwoude.com/choice.php
+
--- Linux/Windows/start.bat
+++ Linux/Windows/start.bat
@@ -9,7 +9,8 @@ cd /d E:\Joy\server-s\dk_nodejs\out     REM NOTE!!! replace it with your own [se
 start C:\Program" "Files\Git\git-bash.exe --cd=E:\Joy\server-s\dk_nodejs\out -c "sh start_manage.sh"
 echo "Successful start shart_manage.sh! GoodLuck~~"
 set filename="%date:~0,4%-%date:~5,2%-%date:~8,2%.log"
-echo "Today's Pvp log: %filename%"
+echo "Today's Pvp log: %filename%; Wait 10s for Pvp server start..."
+CHOICE 	/D Y /T 10	 /M "Do you think 10s has reached?" 
 start C:\Program" "Files\Git\git-bash.exe --cd=E:\Joy\server-s\dk_nodejs\out\pvp_server\logs -c "tail -20f %filename%"
 echo "Successful listen on %filename%! You're ready to rock, Buddy"
 pause
